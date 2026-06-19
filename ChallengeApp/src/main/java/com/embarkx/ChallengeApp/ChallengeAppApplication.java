c9fd21f4d93b845bec1429e47d5a393ba317dbb6
yuvi9770 <140331580+yuvi9770@users.noreply.github.com>
2026-06-19 21:17:24 +0530

    Add files via upload

---
+++ ChallengeApp/src/main/java/com/embarkx/ChallengeApp/ChallengeAppApplication.java
@@ -0,0 +1,12 @@
+package com.embarkx.ChallengeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAppApplication.class, args);
+	}
+}
